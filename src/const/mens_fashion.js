7373ad1d18bab0be876cea75b9cde810e33b9cc5
EmmanuelAbakah <abakah.ema@gmail.com>
2021-12-09 11:34:35 +0000

    commit

---
+++ src/const/mens_fashion.js
@@ -0,0 +1,60 @@
+const plants = [
+    {
+      id: 1,
+      name: 'Long Sleeves Shirt',
+      price: '10.10',
+      like: true,
+      img: require('../assets/image 1.3.png'),
+      about:
+        'This shirt is one of the most cherished shhirt in the market. The color does not make it dirty..',
+    },
+  
+    {
+      id: 2,
+      name: 'African wear',
+      price: '35.50',
+      like: false,
+      img: require('../assets/plant2.png'),
+      about:
+        'This shirt is portrays the african heritage. African symbols and colors are mostly used for it.',
+    },
+    {
+      id: 3,
+      name: 'Casual Floral Short',
+      price: '25.99',
+      like: false,
+      img: require('../assets/image 1.2(2).png'),
+      about:
+        'This shirt helps beautify the environment due to it flamboyancy.It is also easy to wash and handle at all times.',
+    },
+  
+    {
+      id: 4,
+      name: 'Tracksuits',
+      price: '19..99',
+      like: true,
+      img: require('../assets/image 1.4.png'),
+      about:
+        'This shirt can help reduce the scorching sun due to the cap attached. It is also good for cold weather.This is mostly used by young stars for flex.',
+    },
+    {
+      id: 5,
+      name: 'Raincoat ',
+      price: '45.99',
+      like: true,
+      img: require('../assets/image 1.5.png'),
+      about:
+        'As the nams goes, it is really good for cold weather and it is often used by the whites when snow falls to prevent cold.',
+    },
+    {
+      id: 6,
+      name: 'Lacost',
+      price: '50.99',
+      like: false,
+      img: require('../assets/image 1.6.png'),
+      about:
+        'This is one of the cherished shirt for all occations such as parties,outdooring and etc. It also comes with variety of colors to make you glow',
+    },
+  ];
+  
+  export default plants;
